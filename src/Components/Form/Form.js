51d06f5dca8561d5eae7732f1e55fb903885fe89
nfrisbee <nicolefrisbee@yahoo.com>
2019-03-02 12:56:13 -0700

    restarted from scratch on components

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
 
 
-export default Form(props){
-
- return (
-     <div>
-         <div className='defaultImage' style=
-            <form>
-                <label>Image URL</label>
-                    <input placeholder='image URL' 
-                    type='text' 
-                    name='imageURL' 
-                    onChange={props.handleChange} />
-                <label>Product Name</label>
-                    <input placeholder='product name' 
-                    type='text' 
-                    name='productName' 
-                    onChange={props.handleChange} />
-                <label>Price</label>
-                    <input placeholder='price' 
-                    type='text' 
-                    name='price' 
-                    onChange={props.handleChange} />
-                <button className="Cancel" type='reset'>Cancel</button>
-                <button className="Add" type='add'>Add</button>
-             </form>
-     </div>
-);
+class Form extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Hello</div>
+         );
+    }
 }
-
-
-
+ 
+export default Form;
